bf8b788a1ec7a6035909f7476c1f54ecb0abf8ae
Chaitanya <chaitanyapc9@gmail.com>
2026-03-29 17:25:38 +0530

    fix port for render

--- Nodejs/backend/server.js
+++ Nodejs/backend/server.js
@@ -35,6 +35,8 @@ app.get("/leads", (req, res) => {
   res.json(leads);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
